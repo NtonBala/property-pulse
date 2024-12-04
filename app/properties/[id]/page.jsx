bbcd340016a5bbbcf5c0239a9e86dd7221f611d9
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-04 17:36:20 +0200

    Add useParams example

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 
 const PropertyPage = () => {
   const router = useRouter();
+  const params = useParams();
 
   return (
     <div>
+      <h1 className="text-3xl">Property Page</h1>
+      <p>params.id: {params.id}</p>
       <button onClick={() => router.replace('/')}>Go Home</button>
     </div>
   );
