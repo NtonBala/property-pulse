0bae57ef33e8c8281f3fc94e9987d3805ec13ccb
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-05 13:34:44 +0200

    Add usePathname example

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,17 +1,19 @@
 'use client';
 
-import { useRouter, useParams, useSearchParams } from 'next/navigation';
+import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
 
 const PropertyPage = () => {
   const router = useRouter();
   const params = useParams();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
 
   return (
     <div>
       <h1 className="text-3xl">Property Page</h1>
       <p>params.id: {params.id}</p>
       <p>searchParams.name: {searchParams.get('name')}</p>
+      <p>pathname: {pathname}</p>
       <button onClick={() => router.replace('/')}>Go Home</button>
     </div>
   );
