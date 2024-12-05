0f14b3e0d2277a256bfbe0bdce7c1b6b9f4458c0
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-05 22:38:24 +0200

    Revert navigation client hooks examples

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,22 +1,5 @@
-'use client';
-
-import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
-
 const PropertyPage = () => {
-  const router = useRouter();
-  const params = useParams();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-
-  return (
-    <div>
-      <h1 className="text-3xl">Property Page</h1>
-      <p>params.id: {params.id}</p>
-      <p>searchParams.name: {searchParams.get('name')}</p>
-      <p>pathname: {pathname}</p>
-      <button onClick={() => router.replace('/')}>Go Home</button>
-    </div>
-  );
+  return <div>Property Page</div>;
 };
 
 export default PropertyPage;
