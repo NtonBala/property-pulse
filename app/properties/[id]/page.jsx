3dc3959056a4a6792e55c7c152bcb244e617f4f7
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-05 13:29:42 +0200

    Add searchParams example

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,15 +1,17 @@
 'use client';
 
-import { useRouter, useParams } from 'next/navigation';
+import { useRouter, useParams, useSearchParams } from 'next/navigation';
 
 const PropertyPage = () => {
   const router = useRouter();
   const params = useParams();
+  const searchParams = useSearchParams();
 
   return (
     <div>
       <h1 className="text-3xl">Property Page</h1>
       <p>params.id: {params.id}</p>
+      <p>searchParams.name: {searchParams.get('name')}</p>
       <button onClick={() => router.replace('/')}>Go Home</button>
     </div>
   );
