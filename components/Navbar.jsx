728dfeb5a3e15a20036dec633d20524596f8f69b
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-08 19:04:37 +0200

    Add images to Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import logo from '@/assets/images/logo-white.png';
+import profileDefault from '@/assets/images/profile.png';
+
 const Navbar = () => {
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -23,7 +27,7 @@ const Navbar = () => {
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* Logo */}
             <a className="flex flex-shrink-0 items-center" href="/index.html">
-              <img className="h-10 w-auto" src="images/logo-white.png" alt="PropertyPulse" />
+              <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
 
               <span className="hidden md:block text-white text-2xl font-bold ml-2">PropertyPulse</span>
             </a>
@@ -86,7 +90,7 @@ const Navbar = () => {
                 >
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="/images/profile.png" alt="" />
+                  <Image className="h-8 w-8 rounded-full" src={profileDefault} alt="" />
                 </button>
               </div>
 
