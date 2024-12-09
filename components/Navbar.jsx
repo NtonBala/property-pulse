fc51b80aead899691d7048be46c098a7077d16ad
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-09 19:36:53 +0200

    Navbar: fix mobile menu login button icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -177,7 +177,7 @@ const Navbar = () => {
               Add Property
             </Link>
             <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-              <i className="fa-brands fa-google mr-2"></i>
+              <FaGoogle className="text-white mr-2" />
               <span>Login or Register</span>
             </button>
           </div>
